44b1c8bf14500acf6040d353f779c95a07f6c89b
Ryan Hope <rmh3093@gmail.com>
2018-03-26 11:56:02 -0400

    bug fixes and processing improvemts fix endianness for short parsing, cleanup output, add udev rule for ftdi devices, process some unpacked data

--- HondaECU.py
+++ HondaECU.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from pylibftdi import Device
 from struct import unpack
+from tabulate import tabulate
 import time
 import binascii
 
@@ -45,16 +46,52 @@ class HondaECU(Device):
 		
 if __name__ == '__main__':
 	
+	TEMP_OFFSET = -50
+
 	# Initialize communication with ECU
 	ecu = HondaECU()
 	ecu.send([0xfe, 0x04, 0xff, 0xff], False)
 	ecu.send([0x72, 0x05, 0x00, 0xf0, 0x99])
 
-	# Scan tables
-	while True:
-		print("".join("="*160))
-		#for i in range(0,255):
-		for i in [0x00, 0x11, 0x20, 0x61, 0x70, 0xd0, 0xd1]:
-			info = ecu.send_command(0x72, 0x71, i)
-			if info[1] > 0:
-				print(binascii.hexlify(info[0]), info[1], unpack("%dB" % info[1], info[2]))
+	"""
+	Scan tables
+	"""
+	pdata = {}
+	for i in range(0,255):
+	#for i in [0x00, 0x11, 0x20, 0x61, 0x70, 0xd0, 0xd1]:
+		info = ecu.send_command(0x72, 0x71, i)
+		if info[1] > 0:
+			tbl = ord(info[0])
+			print(binascii.hexlify(info[0]), [binascii.hexlify(i) for i in info[2]])
+			if tbl == 0x11 or tbl == 0x61:
+				data = unpack(">H12B3H", info[2])
+				pdata[tbl] = [
+					("RPM", data[0]),
+					("TPS_volt", data[1]*5/256),
+					("TPS_%", data[2]/1.6),
+					("ECT_volt", data[3]*5/256),
+					("ECT_deg_C", data[4]+TEMP_OFFSET),
+					("IAT_volt", data[5]*5/256),
+					("IAT_deg_C", data[6]+TEMP_OFFSET),
+					("MAP_volt", data[7]*5/256),
+					("MAP_kpa", data[8]),
+					("?UNK1", data[9]),
+					("?UNK2", data[10]),
+					("BATT_volt", data[11]/10),
+					("SPEED_kph", data[12]),
+					("IGN_ang", data[13]/10),
+					("*INJ_ms", data[14]),
+					("?UNK3", data[15])
+				]
+			elif tbl == 0xd0:
+				data = unpack(">14B", info[2])
+				pdata[tbl] = [
+					("STARTED", data[1])
+				]
+			else:
+				data = unpack(">%dB" % info[1], info[2])
+	print("")
+	print("")
+	for i,d in pdata.items():
+		print("~~~ %02x ~~~" % i)
+		print(tabulate(d))
