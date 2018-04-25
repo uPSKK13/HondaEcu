7be8808bb8d6fc4425d6e7fff0f6acc3c67e7887
Ryan Hope <rmh3093@gmail.com>
2018-04-25 17:40:22 -0400

    write log to file

--- log.py
+++ log.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('--debug', action='store_true', help="turn on debugging output")
 	parser.add_argument('--table', type=int, default=17, help="table to log")
+	parser.add_argument('--logfile', type=str, default=None, help="file to write log to")
 	args = parser.parse_args()
 
 	ecu = HondaECU()
@@ -24,8 +25,17 @@ if __name__ == '__main__':
 	ecu.send_command([0x72],[0x00, 0xf0], debug=args.debug)
 	print("===============================================")
 
+	log = None
+	if args.logfile != None:
+		log = open(logfile, "w")
 	while True:
 		info = ecu.send_command([0x72], [0x71, args.table], debug=args.debug)
-		data = unpack(">%dB" % len(info[2][2:]) , info[2][2:])
-		print(data)
+		log.write(info[2][2:])
+		log.write("\n")
+		log.flush()
+		if args.debug:
+			data = unpack(">%dB" % len(info[2][2:]) , info[2][2:])
+			print(data)
+	if log != None:
+		log.close()
 	print("===============================================")
