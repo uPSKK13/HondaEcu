0d55109132e2651b36af641fedc141ff9287831f
Ryan Hope <rmh3093@gmail.com>
2018-05-01 13:36:55 -0400

    switch to pytbales/hdf5 storage for log

--- log.py
+++ log.py
@@ -3,17 +3,48 @@
 from __future__ import division, print_function
 from pylibftdi import Device
 from struct import unpack
-from tabulate import tabulate
 import argparse
+import platform
+import datetime
+import time
 
+from tables import *
 from HondaECU import *
+
+get_time = time.time
+if platform.system() == 'Windows':
+    get_time = time.clock
+getTime = get_time
+
+def getDateTimeStamp():
+    d = datetime.datetime.now().timetuple()
+    return "%d-%02.d-%02.d_%02.d-%02.d-%02.d" % (d[0], d[1], d[2], d[3], d[4], d[5])
+
+class HDS_TAB11(IsDescription):
+	timestamp = Float64Col()
+	hds_rpm = UInt16Col()
+	hds_tps_volt = UInt8Col()
+	hds_tps = UInt8Col()
+	hds_ect_volt = UInt8Col()
+	hds_ect = UInt8Col()
+	hds_iat_volt = UInt8Col()
+	hds_iat = UInt8Col()
+	hds_map_volt = UInt8Col()
+	hds_map = UInt8Col()
+	hds_unk1 = UInt8Col()
+	hds_unk2 = UInt8Col()
+	hds_battery_volt = UInt8Col()
+	hds_speed = UInt8Col()
+	hds_ign = UInt8Col()
+	hds_inj = UInt8Col()
+	hds_unk3 = UInt8Col()
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('--debug', action='store_true', help="turn on debugging output")
 	parser.add_argument('--table', type=int, default=17, help="table to log")
-	parser.add_argument('--logfile', type=str, default=None, help="file to write log to")
+	parser.add_argument('--logfile', type=str, default='honda_kline_log.h5', help="log filename")
 	args = parser.parse_args()
 
 	ecu = HondaECU()
@@ -25,16 +56,34 @@ if __name__ == '__main__':
 	ecu.send_command([0x72],[0x00, 0xf0], debug=args.debug)
 	print("===============================================")
 
-	log = None
-	if args.logfile != None:
-		log = open(args.logfile, "w")
+	h5 = open_file("%s_%s.h5" % (args.logfile), mode="w", title="Honda KLine Engine Log")
+	ds = getDateTimeStamp()
+	log = h5.create_table("/", ds, HDS_TAB11, "Log starting on %s" % (ds))
 	while True:
+		t = time.time()
 		info = ecu.send_command([0x72], [0x71, args.table], debug=args.debug)
-		log.write("%f\t" % (time.time()) + info[2][2:] + "\n")
-		log.flush()
+		data = unpack(">H12B3H", info[2][2:])
 		if args.debug:
-			data = unpack(">%dB" % len(info[2][2:]) , info[2][2:])
 			print(data)
-	if log != None:
-		log.close()
+		d = log.row
+		d['timestamp'] = t
+		d['hds_rpm'] = UInt16Col()
+		d['hds_tps_volt'] = UInt8Col()
+		d['hds_tps'] = UInt8Col()
+		d['hds_ect_volt'] = UInt8Col()
+		d['hds_ect'] = UInt8Col()
+		d['hds_iat_volt'] = UInt8Col()
+		d['hds_iat'] = UInt8Col()
+		d['hds_map_volt'] = UInt8Col()
+		d['hds_map'] = UInt8Col()
+		d['hds_unk1'] = UInt8Col()
+		d['hds_unk2'] = UInt8Col()
+		d['hds_battery_volt'] = UInt8Col()
+		d['hds_speed'] = UInt8Col()
+		d['hds_ign'] = UInt16Col()
+		d['hds_inj'] = UInt16Col()
+		d['hds_unk3'] = UInt16Col()
+		d.append()
+		log.flush()
+	h5.close()
 	print("===============================================")
