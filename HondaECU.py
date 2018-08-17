ba395e8446627945c59d7d4594d623ddd9a3c301
Ryan Hope <rmh3093@gmail.com>
2018-08-17 13:25:27 -0400

    lots of cleanups and code refactoring

--- HondaECU.py
+++ HondaECU.py
@@ -5,6 +5,7 @@ from ctypes import *
 import time
 import binascii
 import sys
+import platform
 
 def checksum8bitHonda(data):
 	return ((sum(bytearray(data)) ^ 0xFF) + 1) & 0xFF
@@ -12,8 +13,7 @@ def checksum8bitHonda(data):
 def checksum8bit(data):
 	return 0xff - ((sum(bytearray(data))-1) >> 8)
 
-def validate_checksum(byts, fix=False):
-	cksum = len(byts)-8
+def validate_checksum(byts, cksum, fix=False):
 	fcksum = byts[cksum]
 	ccksum = checksum8bitHonda(byts[:cksum]+byts[(cksum+1):])
 	fixed = False
@@ -33,6 +33,13 @@ def format_message(mtype, data):
 	assert(msg[ml] == len(msg))
 	return msg, ml, dl
 
+class Hex(object):
+	def __call__(self, value):
+		return int(value, 16)
+
+class MaxRetriesException(Exception):
+	pass
+
 class HondaECU(object):
 
 	def __init__(self, device_id=None):
@@ -47,7 +54,8 @@ class HondaECU(object):
 		if self.dev != None:
 			del self.dev
 			self.dev = None
-		self.dev = Device(self.device_id)
+
+		self.dev = Device(self.device_id, auto_detach=(platform.system()=="Windows"))
 		self.starttime = time.time()
 
 	def time(self):
@@ -142,6 +150,7 @@ class HondaECU(object):
 			rdl = ord(rml) - 2 - len(rmtype)
 			rdata = resp[(ml+1):-1]
 			return (rmtype, rml, rdata, rdl)
+		raise MaxRetriesException()
 
 	def do_init_recover(self, debug=False):
 		self.send_command([0x7b], [0x00, 0x01, 0x02], debug=debug)
@@ -150,30 +159,42 @@ class HondaECU(object):
 		self.send_command([0x7b], [0x00, 0x03, 0x75, 0x05, 0x13], debug=debug) # seed/key?
 
 	def do_init_write(self, debug=False):
-		# is this the command to erase the ECU?
-		#self.send_command([0x7d], [0x01, 0x01, 0x00], debug=debug)
-		#self.send_command([0x7d], [0x01, 0x01, 0x01], debug=debug)
 		self.send_command([0x7d], [0x01, 0x01, 0x02], debug=debug)
 		self.send_command([0x7d], [0x01, 0x01, 0x03], debug=debug)
 		self.send_command([0x7d], [0x01, 0x02, 0x50, 0x47, 0x4d], debug=debug) # seed/key?
 		self.send_command([0x7d], [0x01, 0x03, 0x2d, 0x46, 0x49], debug=debug) # seed/key?
 
-	def do_pre_write(self, debug=False):
+	def do_erase(self, debug=False):
 		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
-		time.sleep(14)
+		time.sleep(12)
 		self.send_command([0x7e], [0x01, 0x02], debug=debug)
 		self.send_command([0x7e], [0x01, 0x03, 0x00, 0x00], debug=debug)
 		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
 		self.send_command([0x7e], [0x01, 0x0b, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff], debug=debug) # password?
 		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
 		self.send_command([0x7e], [0x01, 0x0e, 0x01, 0x90], debug=debug)
 		self.send_command([0x7e], [0x01, 0x01, 0x01], debug=debug)
 		self.send_command([0x7e], [0x01, 0x04, 0xff], debug=debug)
 		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
-
-	def do_pre_write_wait(self, debug=False):
 		while True:
 			info = self.send_command([0x7e], [0x01, 0x05], debug=debug)
 			if info[2][1] == 0x00:
 				break
+		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
+	def do_post_write(self, debug=False):
+		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
+		self.send_command([0x7e], [0x01, 0x09], debug=debug)
+		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
+		self.send_command([0x7e], [0x01, 0x0a], debug=debug)
+		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
+		self.send_command([0x7e], [0x01, 0x0c], debug=debug)
+		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
+
+		self.send_command([0x7e], [0x01, 0x0d], debug=debug)
 		self.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
--- flash.py
+++ flash.py
@@ -9,23 +9,26 @@ import argparse
 
 from HondaECU import *
 
-def do_validation(binfile, fix=False):
-	print("==============================================================================")
+def print_header():
+	sys.stdout.write("===================================================\n")
+
+def do_validation(binfile, cksum, fix=False):
+	print_header()
 	if fix:
-		print("Fixing bin file checksum")
+		sys.stdout.write("Fixing bin file checksum\n")
 	else:
-		print("Validating bin file checksum")
+		sys.stdout.write("Validating bin file checksum\n")
 	with open(binfile, "rb") as fbin:
-		byts, fcksum, ccksum, fixed = validate_checksum(bytearray(fbin.read(os.path.getsize(binfile))), fix)
+		byts, fcksum, ccksum, fixed = validate_checksum(bytearray(fbin.read(os.path.getsize(binfile))), cksum, fix)
 		if fixed:
 			status = "fixed"
 		elif fcksum == ccksum:
 			status = "good"
 		else:
 			status = "bad"
-		print("  file checksum: %s" % fcksum)
-		print("  calculated checksum: %s" % ccksum)
-		print("  status: %s" % status)
+		sys.stdout.write("  file checksum: %s\n" % fcksum)
+		sys.stdout.write("  calculated checksum: %s\n" % ccksum)
+		sys.stdout.write("  status: %s\n" % status)
 		return byts, fcksum, ccksum, fixed
 
 def do_read_flash(ecu, binfile, rom_size, debug=False):
@@ -39,11 +42,11 @@ def do_read_flash(ecu, binfile, rom_size, debug=False):
 			fbin.write(info[2])
 			fbin.flush()
 			nbyte += readsize
-			if nbyte % 64 == 0:
+			if nbyte % 256 == 0:
 				sys.stdout.write(".")
-			if nbyte % 1024 == 0:
+			if nbyte % 8192 == 0:
 				n = time.time()
-				sys.stdout.write(" %dkB %.02fBps\n" % (int(nbyte/1024),1024/(n-t)))
+				sys.stdout.write(" %dkB %.02fBps\n" % (int(nbyte/1024),8192/(n-t)))
 				t = n
 			sys.stdout.flush()
 
@@ -70,37 +73,24 @@ def do_write_flash(ecu, byts, debug=False, offset=0):
 		i += 1
 		if i % 2 == 0:
 			ecu.send_command([0x7e], [0x01, 0x08], debug=debug)
-		sys.stdout.write(".")
-		if i % 64 == 0:
+		if i % 4 == 0:
+			sys.stdout.write(".")
+		if i % 128 == 0:
 			n = time.time()
 			w = (i*writesize)
-			sys.stdout.write(" %dkB %.02fBps\n" % (int(w/1024),(64*128)/(n-t)))
+			sys.stdout.write(" %4dkB %.02fBps\n" % (int(w/1024),(128*128)/(n-t)))
 			t = n
 		sys.stdout.flush()
-
-def do_post_write(ecu, debug=False):
-	time.sleep(3)
-	ecu.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x09], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x0a], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x0c], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x0d], debug=debug)
-	ecu.send_command([0x7e], [0x01, 0x01, 0x00], debug=debug)
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('mode', choices=["read","write","recover","validate_checksum","fix_checksum"], help="ECU mode")
 	parser.add_argument('binfile', help="name of bin to read or write")
-	parser.add_argument('--write-mode', type=int, choices=[0,1], default=0, help="ECU write technique")
-	parser.add_argument('--recover-mode', type=int, choices=[0,1], default=0, help="ECU recover technique")
-	parser.add_argument('--rom-size', default=256, type=int, help="size of ECU rom in kilobytes")
+	parser.add_argument('--checksum', default='0x3fff8', type=Hex(), help="hex location of checksum in ECU bin")
+	parser.add_argument('--rom-size', default=256, type=int, help="size of ECU bin in kilobytes")
+	parser.add_argument('--fix-checksum', action='store_true', help="fix checksum before write/recover")
 	db_grp = parser.add_argument_group('debugging options')
-	db_grp.add_argument('--skip-power-check', action='store_true', help="don't test for k-line activity")
-	db_grp.add_argument('--fix-checksum', action='store_true', help="fix checksum before write/recover")
 	db_grp.add_argument('--debug', action='store_true', help="turn on debugging output")
 	args = parser.parse_args()
 
@@ -113,7 +103,7 @@ if __name__ == '__main__':
 
 	ret = 1
 	if args.mode != "read":
-		byts, fcksum, ccksum, fixed = do_validation(binfile, args.mode == "fix_checksum" or args.fix_checksum)
+		byts, fcksum, ccksum, fixed = do_validation(binfile, args.checksum, args.mode == "fix_checksum" or args.fix_checksum)
 		if (fcksum == ccksum or fixed) and args.mode in ["recover","write"]:
 			ret = 0
 		else:
@@ -126,52 +116,73 @@ if __name__ == '__main__':
 		ecu = HondaECU()
 		ecu.setup()
 
-		if not args.skip_power_check:
-			print("===============================================")
-			# In order to read flash we must send commands while the ECU's
-			# bootloader is still active which is right after the bike powers up
-			if ecu.kline():
-				print("Turn off bike")
-				while ecu.kline():
-					time.sleep(.1)
-			print("Turn on bike")
-			while not ecu.kline():
+		print_header()
+		if ecu.kline():
+			sys.stdout.write("Turn off bike\n")
+			while ecu.kline():
 				time.sleep(.1)
-			time.sleep(.5)
+		sys.stdout.write("Turn on bike\n")
+		while not ecu.kline():
+			time.sleep(.1)
+		time.sleep(.5)
 
-		print("===============================================")
-		print("Initializing ECU communications")
-		ecu.init(debug=args.debug)
+		print_header()
+		sys.stdout.write("Wake-up ECU\n")
+		try:
+			initok = ecu.init(debug=args.debug)
+			print_header()
+			sys.stdout.write("Entering diagnostic mode\n")
+			ecu.send_command([0x72],[0x00, 0xf0], debug=args.debug)
+		except MaxRetriesException:
+			initok = False
+		except:
+			sys.exit(-1)
 
 		if args.mode == "read":
-			print("===============================================")
-			print("Reading ECU")
-			ecu.send_command([0x72],[0x00, 0xf0], debug=args.debug)
+			print_header()
+			sys.stdout.write("Security access\n")
 			ecu.send_command([0x27],[0xe0, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x48, 0x6f], debug=args.debug)
 			ecu.send_command([0x27],[0xe0, 0x77, 0x41, 0x72, 0x65, 0x59, 0x6f, 0x75], debug=args.debug)
+
+			print_header()
+			sys.stdout.write("Reading ECU\n")
 			do_read_flash(ecu, binfile, args.rom_size, debug=args.debug)
-			do_validation(binfile)
+			do_validation(binfile, args.checksum)
 
-		elif args.mode == "write":
-			print("===============================================")
-			print("Writing ECU")
-			ecu.send_command([0x72],[0x00, 0xf0], debug=args.debug)
-			ecu.do_init_write(debug=args.debug)
-			ecu.do_pre_write(debug=args.debug)
-			ecu.do_pre_write_wait(debug=args.debug)
+		else:
+			if args.mode == "write":
+				print_header()
+				sys.stdout.write("Initializing write process\n")
+				try:
+					ecu.do_init_write(debug=args.debug)
+				except MaxRetriesException:
+					args.mode = "recover"
+					sys.stdout.write("Switching to recovery mode\n")
+				except:
+					sys.exit(-1)
+
+			if args.mode == "recover":
+				if initok:
+					print_header()
+					sys.stdout.write("Initializing recovery process\n")
+					ecu.do_init_recover(debug=args.debug)
+
+					print_header()
+					sys.stdout.write("Entering enhanced diagnostic mode\n")
+					ecu.send_command([0x72],[0x00, 0xf1], debug=args.debug)
+					ecu.send_command([0x27],[0x00, 0x9f, 0x00], debug=args.debug)
+
+			print_header()
+			sys.stdout.write("Erasing ECU\n")
+			ecu.do_erase(debug=args.debug)
+
+			print_header()
+			sys.stdout.write("Writing ECU\n")
 			do_write_flash(ecu, byts, offset=0, debug=args.debug)
-			do_post_write(ecu, debug=args.debug)
 
-		elif args.mode == "recover":
-			print("===============================================")
-			print("Recovering ECU")
-			ecu.do_init_recover(debug=args.debug)
-			ecu.send_command([0x72],[0x00, 0xf1], debug=args.debug)
-			ecu.send_command([0x27],[0x00, 0x9f, 0x00], debug=args.debug)
-			ecu.do_pre_write(debug=args.debug)
-			ecu.do_pre_write_wait(debug=args.debug)
-			do_write_flash(ecu, byts, offset=0, debug=args.debug)
-			do_post_write(ecu, debug=args.debug)
+			print_header()
+			sys.stdout.write("Finalizing write process\n")
+			ecu.do_post_write(debug=args.debug)
 
-	print("===============================================")
+	print_header()
 	sys.exit(ret)
