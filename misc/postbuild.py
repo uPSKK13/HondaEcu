42dc4fa8e84c62c85f15a3fc514ee2790fb963b0
Ryan Hope <rmh3093@gmail.com>
2019-02-17 13:21:18 -0500

    tweak exe filenames

--- misc/postbuild.py
+++ misc/postbuild.py
@@ -6,6 +6,7 @@ r = git.repo.Repo()
 v = r.git.describe("--tags")
 for f in glob.glob("dist/*.exe"):
     fn = list(os.path.splitext(f))
+    fn[0] += "-"
     fn[0] += v
     fn = "".join(fn)
     os.rename(f,fn)
