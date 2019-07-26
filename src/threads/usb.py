bb8c007c5dcf8172a78062f87a4073c779afe575
Ryan Hope <rmh3093@gmail.com>
2019-07-26 03:39:46 -0400

    different driver backend

--- src/threads/usb.py
+++ src/threads/usb.py
@@ -1,6 +1,8 @@
 import sys
 import time
 import wx
+import usb.backend.libusb1
+from ctypes import c_void_p, c_int
 import usb.core
 import usb.util
 import pyftdi.ftdi
@@ -12,13 +14,21 @@ class USBMonitor(Thread):
 	def __init__(self, parent):
 		self.parent = parent
 		self.ftdi_devices = {}
+		self.backend = usb.backend.libusb1.get_backend(find_library=lambda x: "libusb-1.0.dll")
+		if self.backend:
+			self.backend.lib.libusb_set_option.argtypes = [c_void_p, c_int]
+			self.backend.lib.libusb_set_option(self.backend.ctx, 1)
 		Thread.__init__(self)
 
 	def run(self):
 		while self.parent.run:
 			time.sleep(.5)
 			new_devices = {}
-			for cfg in usb.core.find(find_all=True, idVendor=pyftdi.ftdi.Ftdi.FTDI_VENDOR):
+			if self.backend:
+				devices = usb.core.find(find_all=True, idVendor=pyftdi.ftdi.Ftdi.FTDI_VENDOR, backend=self.backend)
+			else:
+				devices = usb.core.find(find_all=True, idVendor=pyftdi.ftdi.Ftdi.FTDI_VENDOR)
+			for cfg in devices:
 				device = "%03d:%03d" % (cfg.bus,cfg.address)
 				new_devices[device] = cfg
 				if not device in self.ftdi_devices:
